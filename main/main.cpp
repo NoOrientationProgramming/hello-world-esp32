6f6729aa0deca80e809e7ad31917cceac5ce9b41
Johannes Natter <office@dsp-crowd.com>
2024-02-09 13:49:15 +0100

    main: Make scheduling fair compared to worker threads

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,9 @@
 
+#include <thread>
+
 #include "WlvlSupervising.h"
+
+using namespace std;
 
 extern "C" void app_main()
 {
@@ -8,6 +12,9 @@ extern "C" void app_main()
 	levelLogSet(5);
 
 	while (1)
+	{
 		pApp->treeTick();
+		this_thread::sleep_for(chrono::milliseconds(2));
+	}
 }
 
